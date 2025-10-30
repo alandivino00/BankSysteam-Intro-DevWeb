ede092a797c9d6adc44f1bba9e270971784fc318
alandivino00 <38597412+alandivino00@users.noreply.github.com>
2025-10-30 09:51:47 -0300

    Add project files.

---
+++ BankSysteam.Api/Controllers/ContasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankSysteam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/contas")]
+    public class ContasController : ControllerBase
+    {
+        // Lista em memória simulando dados do banco
+        private static readonly List<string> contas = new List<string>
+        {
+            "Conta Corrente - João",
+            "Conta Poupança - Maria",
+            "Conta Salário - Ana"
+        };
+
+        // Ação GET: retorna a lista de contas
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetContas()
+        {
+            return Ok(contas);
+        }
+    }
+}
---
+++ BankSysteam.Api/Program.cs
@@ -0,0 +1,35 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        //builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
